dd730baaa2a98dff9e93b80380ba670c74dda4fb
Sean Kim <skim2636@gmail.com>
2015-03-05 19:34:00 -0800

    wiring in mongodb

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,53 @@
-var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+
+var userSchema = mongoose.Schema({
+  username: {type: STRING, required: true, index: {unique: true}},
+  password: {type: STRING, required: true}
 });
+
+var User = mongoose.model('User', userSchema);
+
+User.comparePassword = function(enteredPassword, savedPassword, cb) {
+  bcrypt.compare(enteredPassword, savedPassword, function(err, matched) {
+    if (err) {
+      return cb(err);
+    } else {
+      cb(null, matched);
+    }
+  });
+};
+
+userSchema.pre('save', function(next) {
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next():
+    });
+});
+
+//// sqlite3
+//var User = db.Model.extend({
+//  tableName: 'users',
+//  hasTimestamps: true,
+//  initialize: function(){
+//    this.on('creating', this.hashPassword);
+//  },
+//  comparePassword: function(attemptedPassword, callback) {
+//    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//      callback(isMatch);
+//    });
+//  },
+//  hashPassword: function(){
+//    var cipher = Promise.promisify(bcrypt.hash);
+//    return cipher(this.get('password'), null, null).bind(this)
+//      .then(function(hash) {
+//        this.set('password', hash);
+//      });
+//  }
+//});
 
 module.exports = User;
